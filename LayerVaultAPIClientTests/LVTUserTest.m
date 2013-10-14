208fe7caacd545063f8a6fc195364f582023e7f8
Matt Thomas <matt@codecaffeine.com>
2013-10-14 18:06:52 -0400

    Adding some tests

---
+++ LayerVaultAPIClientTests/LVTUserTest.m
@@ -0,0 +1,37 @@
+//
+//  LVTUserTest.m
+//  LayerVaultAPIClient
+//
+//  Created by Matt Thomas on 10/14/13.
+//  Copyright (c) 2013 codecaffeine. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <Mantle/Mantle.h>
+#import "LVTUser.h"
+
+@interface LVTUserTest : XCTestCase
+
+@end
+
+@implementation LVTUserTest
+
+- (void)testNilJSONNilUser
+{
+    LVTUser *user = [MTLJSONAdapter modelOfClass:LVTUser.class
+                              fromJSONDictionary:nil
+                                           error:nil];
+    XCTAssertNil(user, @"user should be nil");
+}
+
+
+- (void)testNoAdminMakesAdminFalse
+{
+    LVTUser *user = [MTLJSONAdapter modelOfClass:LVTUser.class
+                              fromJSONDictionary:@{}
+                                           error:nil];
+    XCTAssertNotNil(user, @"user shouldn't be nil");
+    XCTAssertFalse(user.admin, @"Should not be admin");
+}
+
+@end
