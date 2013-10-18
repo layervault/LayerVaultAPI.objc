90d9e41fcff0463dc43604e161c3e0cf76132e82
Matt Thomas <matt@codecaffeine.com>
2013-10-17 21:45:46 -0400

    Adding Constants file

---
+++ LayerVaultAPIClient/LVConstants.h
@@ -0,0 +1,17 @@
+//
+//  LVConstants.h
+//  LayerVaultAPIClient
+//
+//  Created by Matt Thomas on 10/17/13.
+//  Copyright (c) 2013 codecaffeine. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#ifndef LayerVaultAPIClient_LVConstants_h
+#define LayerVaultAPIClient_LVConstants_h
+
+NSString *const LVClientID = @"CLIENTID";
+NSString *const LVClientSecret = @"CLIENTSECRET";
+
+#endif
