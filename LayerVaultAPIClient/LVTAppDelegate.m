ea43d7a2a697e701d6eb0ec8a07a95fb850269af
Matt Thomas <matt@codecaffeine.com>
2013-10-18 12:58:30 -0400

    Using secure field Adding button action adding login logic

--- LayerVaultAPIClient/LVTAppDelegate.m
+++ LayerVaultAPIClient/LVTAppDelegate.m
@@ -11,13 +11,15 @@
 #import "LVTUser.h"
 #import <ReactiveCocoa/ReactiveCocoa.h>
 #import "LVConstants.h"
+#import <Mantle/EXTScope.h>
 
 @interface LVTAppDelegate ()
 @property (nonatomic) LVTHTTPClient *client;
 @property (weak) IBOutlet NSTextField *loggedInLabel;
 @property (weak) IBOutlet NSTextField *emailField;
-@property (weak) IBOutlet NSTextField *passwordField;
+@property (weak) IBOutlet NSSecureTextField *passwordField;
 @property (weak) IBOutlet NSButton *loginButton;
+@property (nonatomic) AFOAuthCredential *credential;
 @end
 
 @implementation LVTAppDelegate
@@ -30,24 +32,75 @@
     [RACObserve(self.client, user) subscribeNext:^(LVTUser *user) {
         if (user.email) {
             [self.loggedInLabel setStringValue:user.email];
+            NSLog(@"logged in as: %@", user);
         }
         else {
             self.loggedInLabel.stringValue = NSLocalizedString(@"Not Logged In", nil);
         }
     }];
 
-    AFOAuthCredential *credential = [AFOAuthCredential retrieveCredentialWithIdentifier:self.client.serviceProviderIdentifier];
-    if (credential) {
-        [self.client setAuthorizationHeaderWithCredential:credential];
-        [self.client authenticateWithBlock:^(LVTUser *user, NSError *error, AFHTTPRequestOperation *operation) {
-            if (!user && error) {
-                [AFOAuthCredential deleteCredentialWithIdentifier:self.client.serviceProviderIdentifier];
+    @weakify(self);
+    [RACObserve(self, credential) subscribeNext:^(AFOAuthCredential *credential) {
+        @strongify(self);
+        if (credential) {
+            if (credential.isExpired) {
+                @weakify(self);
+                [self.client authenticateUsingOAuthWithPath:@"/oauth/token"
+                                               refreshToken:credential.refreshToken
+                                                    success:^(AFOAuthCredential *credential) {
+                                                        @strongify(self);
+                                                        self.credential = credential;
+                                                    }
+                                                    failure:^(NSError *error) {
+                                                        @strongify(self);
+                                                        self.credential = nil;
+                                                    }];
             }
-        }];
-    }
-    else {
-        
-    }
+            else {
+                [AFOAuthCredential storeCredential:credential
+                                    withIdentifier:self.client.serviceProviderIdentifier];
+
+                [self.emailField setEnabled:NO];
+                self.emailField.stringValue = @"";
+                [self.passwordField setEnabled:NO];
+                self.passwordField.stringValue = @"";
+                [self.loginButton setEnabled:NO];
+                
+                [[self.client fetchUserInfo] subscribeError:^(NSError *error) {
+                    self.credential = nil;
+                }];
+            }
+        }
+        else {
+            [AFOAuthCredential deleteCredentialWithIdentifier:self.client.serviceProviderIdentifier];
+            [self.emailField setEnabled:YES];
+            [self.emailField becomeFirstResponder];
+            [self.passwordField setEnabled:YES];
+            [self.loginButton setEnabled:YES];
+        }
+    }];
+
+
+    self.credential = [AFOAuthCredential retrieveCredentialWithIdentifier:self.client.serviceProviderIdentifier];
 }
+
+- (IBAction)loginPressed:(NSButton *)sender
+{
+    @weakify(self)
+    [self.client authenticateUsingOAuthWithPath:@"/oauth/token"
+                                       username:self.emailField.stringValue
+                                       password:self.passwordField.stringValue
+                                          scope:nil
+                                        success:^(AFOAuthCredential *credential) {
+                                            @strongify(self);
+                                            NSLog(@"I have a token! %@", credential);
+                                            self.credential = credential;
+                                        }
+                                        failure:^(NSError *error) {
+                                            @strongify(self);
+                                            self.credential = nil;
+                                        }];
+}
+
 
 @end
