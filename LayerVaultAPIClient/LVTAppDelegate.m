bc4462a1e1d57e19018467731ed17511d08d0527
Matt Thomas <matt@codecaffeine.com>
2013-10-17 21:49:22 -0400

    Add outlets and use updated API

--- LayerVaultAPIClient/LVTAppDelegate.m
+++ LayerVaultAPIClient/LVTAppDelegate.m
@@ -9,22 +9,41 @@
 #import "LVTAppDelegate.h"
 #import "LVTHTTPClient.h"
 #import "LVTUser.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
+#import "LVConstants.h"
 
 @interface LVTAppDelegate ()
 @property (nonatomic) LVTHTTPClient *client;
+@property (weak) IBOutlet NSTextField *loggedInLabel;
+@property (weak) IBOutlet NSTextField *emailField;
+@property (weak) IBOutlet NSTextField *passwordField;
+@property (weak) IBOutlet NSButton *loginButton;
 @end
 
 @implementation LVTAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    self.client = [[LVTHTTPClient alloc] initWithToken:@"87043616ba2462339ee11548c00e37b14a3480b06ed7532108d6d30e52065f59"];
+    self.client = [[LVTHTTPClient alloc] initWithClientID:LVClientID
+                                                   secret:LVClientSecret];
 
-    [self.client getMyInfo:^(LVTUser *user, NSError *error, AFHTTPRequestOperation *operation) {
-        NSLog(@"user: %@", user);
-        NSLog(@"error: %@", error);
-        NSLog(@"operation: %@", operation);
+    [RACObserve(self.client, user) subscribeNext:^(LVTUser *user) {
+        if (user.email) {
+            [self.loggedInLabel setStringValue:user.email];
+        }
+        else {
+            self.loggedInLabel.stringValue = NSLocalizedString(@"Not Logged In", nil);
+        }
     }];
+
+    AFOAuthCredential *credential = [AFOAuthCredential retrieveCredentialWithIdentifier:self.client.serviceProviderIdentifier];
+    if (credential) {
+        [self.client setAuthorizationHeaderWithCredential:credential];
+        [self.client getMyInfo:nil];
+    }
+    else {
+        
+    }
 }
 
 @end
