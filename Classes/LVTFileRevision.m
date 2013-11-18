a2dcbb54275066b5b6d231fcac8510aff93c5a3c
Matt Thomas <matt@codecaffeine.com>
2013-11-18 09:04:31 -0500

    Adding LVTFileRevision

---
+++ Classes/LVTFileRevision.m
@@ -0,0 +1,76 @@
+//
+//  LVTFileRevision.m
+//  LayerVaultAPIClient
+//
+//  Created by Matt Thomas on 11/18/13.
+//  Copyright (c) 2013 codecaffeine. All rights reserved.
+//
+
+#import "LVTFileRevision.h"
+
+@implementation LVTFileRevision
+
++ (NSDateFormatter *)dateFormatter
+{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
+    dateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss'Z'";
+    return dateFormatter;
+}
+
+
++ (NSDictionary *)JSONKeyPathsByPropertyKey
+{
+    return @{@"revision": @"revision_number",
+             @"md5": @"md5",
+             @"dateCreated": @"created_at",
+             @"dateUpdated": @"updated_at",
+             @"webURL": @"full_url",
+             @"downloadURL": @"download_url",
+             @"shortenedURL": @"shortened_url"};
+}
+
+
++ (NSValueTransformer *)dateCreatedJSONTransformer
+{
+    return [MTLValueTransformer
+            reversibleTransformerWithForwardBlock:^NSDate *(NSString *string) {
+                return [[self dateFormatter] dateFromString:string];
+            }
+            reverseBlock:^NSString *(NSDate *date) {
+                return [[self dateFormatter] stringFromDate:date];
+            }];
+}
+
+
++ (NSValueTransformer *)dateUpdatedJSONTransformer
+{
+    return [MTLValueTransformer
+            reversibleTransformerWithForwardBlock:^NSDate *(NSString *string) {
+                return [[self dateFormatter] dateFromString:string];
+            }
+            reverseBlock:^NSString *(NSDate *date) {
+                return [[self dateFormatter] stringFromDate:date];
+            }];
+}
+
+
++ (NSValueTransformer *)shortURLJSONTransformer
+{
+    return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
+}
+
+
++ (NSValueTransformer *)downloadURLJSONTransformer
+{
+    return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
+}
+
+
++ (NSValueTransformer *)shortenedURLJSONTransformer
+{
+    return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
+}
+
+
+@end
