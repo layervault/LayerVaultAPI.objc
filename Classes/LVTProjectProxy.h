bf3a216e36071379a2e59b55334f20f94f2f16cb
Matt Thomas <matt@codecaffeine.com>
2013-12-02 14:34:45 -0500

    Add needsCreation to ProjectProxy
    denotes a proxy that isn’t created on the server yet

--- Classes/LVTProjectProxy.h
+++ Classes/LVTProjectProxy.h
@@ -14,6 +14,7 @@
 @property (readonly, copy) NSString *name;
 @property (readonly, copy) NSString *organizationPermalink;
 @property (nonatomic) LVTProject *futureProject;
+@property (nonatomic) BOOL needsCreation;
 
 - (instancetype)initWithName:(NSString *)name
        organizationPermalink:(NSString *)organizationPermalink;
