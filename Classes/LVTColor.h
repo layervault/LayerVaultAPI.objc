a78e93f2cfc8f0a17500bd4afb9ec6bd5ffafdd6
Matt Thomas <matt@codecaffeine.com>
2013-12-03 13:00:53 -0500

    Adding a color class

---
+++ Classes/LVTColor.h
@@ -0,0 +1,22 @@
+//
+//  LVTColor.h
+//  LayerVaultAPIClient
+//
+//  Created by Matt Thomas on 12/3/13.
+//  Copyright (c) 2013 codecaffeine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef enum : NSUInteger {
+    LVTColorWhite = 0,
+    LVTColorGreen = 2,
+    LVTColorRed = 6,
+    LVTColorOrange = 7
+} LVTColorValues;
+
+@interface LVTColor : NSObject
+
++ (NSDictionary *)colorNamesToValue;
+
+@end
---
+++ Classes/LVTColor.m
@@ -0,0 +1,21 @@
+//
+//  LVTColor..
+//  LayerVaultAPIClient
+//
+//  Created by Matt Thomas on 12/3/13.
+//  Copyright (c) 2013 codecaffeine. All rights reserved.
+//
+
+#import "LVTColor.h"
+
+@implementation LVTColorUtils
+
++ (NSDictionary *)colorNamesToValue
+{
+    return @{@"white": @(LVTColorWhite),
+             @"green": @(LVTColorGreen),
+             @"red": @(LVTColorRed),
+             @"orange": @(LVTColorOrange)};
+}
+
+@end
