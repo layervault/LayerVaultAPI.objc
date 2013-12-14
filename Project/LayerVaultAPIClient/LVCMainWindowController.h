eab632787e00a99554950cacae2c87917a48ccdd
Matt Thomas <matt@codecaffeine.com>
2013-12-14 12:23:26 -0500

    Adding MainWindowController

---
+++ Project/LayerVaultAPIClient/LVCMainWindowController.h
@@ -0,0 +1,13 @@
+//
+//  LVCMainWindowController.h
+//  LayerVaultAPIClient
+//
+//  Created by Matt Thomas on 12/14/13.
+//  Copyright (c) 2013 LayerVault. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface LVCMainWindowController : NSWindowController
+
+@end
---
+++ Project/LayerVaultAPIClient/LVCMainWindowController.m
@@ -0,0 +1,33 @@
+//
+//  LVCMainWindowController.m
+//  LayerVaultAPIClient
+//
+//  Created by Matt Thomas on 12/14/13.
+//  Copyright (c) 2013 LayerVault. All rights reserved.
+//
+
+#import "LVCMainWindowController.h"
+
+@interface LVCMainWindowController ()
+
+@end
+
+@implementation LVCMainWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
