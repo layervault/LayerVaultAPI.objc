a84c77e55da022bdfd9125772657a21701792a50
Matt Thomas <matt@codecaffeine.com>
2013-12-15 12:43:47 -0500

    Use OrganizationsViewController

--- Project/LayerVaultAPIClient/LVCMainWindowController.m
+++ Project/LayerVaultAPIClient/LVCMainWindowController.m
@@ -7,11 +7,14 @@
 //
 
 #import "LVCMainWindowController.h"
+#import "LVCOrganizationsViewController.h"
 #import <layervault_objc_client/LayerVaultAPI.h>
 
 @interface LVCMainWindowController () <NSOutlineViewDataSource, NSOutlineViewDelegate>
 @property (weak) IBOutlet NSOutlineView *sourceList;
 @property (weak) IBOutlet NSOutlineView *fileList;
+@property (strong) IBOutlet LVCOrganizationsViewController *organizationsViewController;
+@property (weak) IBOutlet NSView *sourceViewContainer;
 @end
 
 @implementation LVCMainWindowController
@@ -29,7 +32,74 @@
 {
     [super windowDidLoad];
     
-    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    self.organizationsViewController.organizations = self.user.organizations;
+    NSView *contentView = self.organizationsViewController.view;
+    contentView.autoresizingMask = NSViewMinXMargin|NSViewWidthSizable|NSViewMaxXMargin|NSViewMinYMargin|NSViewHeightSizable|NSViewMaxYMargin;
+    contentView.frame = self.sourceViewContainer.bounds;
+    [self.sourceViewContainer addSubview:contentView];
 }
+
+#pragma mark - NSOutlineViewDataSource
+- (NSInteger)outlineView:(NSOutlineView *)outlineView numberOfChildrenOfItem:(id)item
+{
+    if (item) {
+        if ([item isKindOfClass:LVCOrganization.class]) {
+            LVCOrganization *organization = (LVCOrganization *)item;
+            NSPredicate *predicate = [NSPredicate predicateWithFormat:@"member = TRUE"];
+            NSArray *filteredArray = [organization.projects filteredArrayUsingPredicate:predicate];
+            return filteredArray.count;
+        }
+        return 0;
+    }
+    return self.user.organizations.count;
+}
+
+
+- (BOOL)outlineView:(NSOutlineView *)outlineView isItemExpandable:(id)item
+{
+    return [item isKindOfClass:LVCOrganization.class];
+}
+
+- (id)outlineView:(NSOutlineView *)outlineView child:(NSInteger)index ofItem:(id)item
+{
+    if (item) {
+        if ([item isKindOfClass:LVCOrganization.class]) {
+            LVCOrganization *organization = (LVCOrganization *)item;
+            NSPredicate *predicate = [NSPredicate predicateWithFormat:@"member = TRUE"];
+            NSArray *filteredArray = [organization.projects filteredArrayUsingPredicate:predicate];
+            return filteredArray[index];
+        }
+        return nil;
+    }
+    return self.user.organizations[index];
+}
+
+
+#pragma mark - NSOutlineViewDelegate
+- (NSView *)outlineView:(NSOutlineView *)outlineView viewForTableColumn:(NSTableColumn *)tableColumn item:(id)item
+{
+    NSTableCellView *tableCellView = nil;
+    if (item) {
+        if ([item isKindOfClass:LVCOrganization.class]) {
+            LVCOrganization *organization = (LVCOrganization *)item;
+            tableCellView = [outlineView makeViewWithIdentifier:@"HeaderCell"
+                                                          owner:self];
+            [tableCellView.textField setStringValue:organization.name];
+        }
+        else if ([item isKindOfClass:LVCProject.class]) {
+            LVCProject *project = (LVCProject *)item;
+            tableCellView = [outlineView makeViewWithIdentifier:@"DataCell"
+                                                          owner:self];
+            [tableCellView.textField setStringValue:project.name];
+        }
+    }
+    return tableCellView;
+}
+
+
+- (BOOL)outlineView:(NSOutlineView *)outlineView shouldShowOutlineCellForItem:(id)item {
+    return NO;
+}
+
 
 @end
