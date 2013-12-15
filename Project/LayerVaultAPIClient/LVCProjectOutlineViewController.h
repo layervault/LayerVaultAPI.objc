bf8b37f2e56d96486f7a1de48013ca4f89d39584
Matt Thomas <matt@codecaffeine.com>
2013-12-15 12:52:46 -0500

    Add LCVProjectOutlineViewController

---
+++ Project/LayerVaultAPIClient/LVCProjectOutlineViewController.h
@@ -0,0 +1,14 @@
+//
+//  LVCProjectOutlineViewController.h
+//  LayerVaultAPIClient
+//
+//  Created by Matt Thomas on 12/15/13.
+//  Copyright (c) 2013 LayerVault. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+@class LVCProject;
+
+@interface LVCProjectOutlineViewController : NSViewController
+@property (nonatomic) LVCProject *project;
+@end
---
+++ Project/LayerVaultAPIClient/LVCProjectOutlineViewController.m
@@ -0,0 +1,27 @@
+//
+//  LVCProjectOutlineViewController.m
+//  LayerVaultAPIClient
+//
+//  Created by Matt Thomas on 12/15/13.
+//  Copyright (c) 2013 LayerVault. All rights reserved.
+//
+
+#import "LVCProjectOutlineViewController.h"
+#import <layervault_objc_client/LayerVaultAPI.h>
+
+@interface LVCProjectOutlineViewController ()
+
+@end
+
+@implementation LVCProjectOutlineViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+@end
