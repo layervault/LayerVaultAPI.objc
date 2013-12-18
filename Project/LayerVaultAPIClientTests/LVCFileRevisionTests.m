b7dcb27cb7d76786b02a56823ffc4f29d5fc7b74
Matt Thomas <matt@codecaffeine.com>
2013-12-18 17:06:03 -0500

    Test File Revision

--- Project/LayerVaultAPIClientTests/LVCFileRevisionTests.m
+++ Project/LayerVaultAPIClientTests/LVCFileRevisionTests.m
@@ -7,9 +7,11 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <LayerVaultAPI/LVCFile.h>
+#import <LayerVaultAPI/LVCFileRevision.h>
 
 @interface LVCFileRevisionTests : XCTestCase
-
+@property (nonatomic, copy) NSDictionary *validJSON;
 @end
 
 @implementation LVCFileRevisionTests
@@ -17,18 +19,64 @@
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.validJSON = @{@"revision_number": @(4),
+                       @"md5": @"DEADBEEF",
+                       @"created_at": @"2013-11-10T01:23:45Z",
+                       @"updated_at": @"2013-11-20T20:11:10Z",
+                       @"full_url": @"https://layervault.com/codecaffeine/awesome-sauce/oxchitl/4",
+                       @"download_url": @"https://layervault.com/files/download_node/DEADBEEF",
+                       @"shortened_url": @"http://lyrv.lt/DEADBEEF"};
 }
 
-- (void)tearDown
+
+- (void)testFileExcludedFromEquality
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+    LVCFile *file1 = [MTLJSONAdapter modelOfClass:LVCFile.class
+                              fromJSONDictionary:@{@"name": @"oxchitl"}
+                                           error:nil];
+    XCTAssertNotNil(file1, @"file should not be nil");
+
+    LVCFileRevision *revision1File1 = [MTLJSONAdapter modelOfClass:LVCFileRevision.class
+                                                fromJSONDictionary:self.validJSON
+                                                             error:nil];
+    revision1File1.file = file1;
+    XCTAssertNotNil(revision1File1, @"revision1File1 should not be nil");
+    XCTAssertNotNil(revision1File1.file, @"should have parentFolder");
+
+    LVCFile *file2 = [MTLJSONAdapter modelOfClass:LVCFile.class
+                               fromJSONDictionary:@{@"name": @"sriracha"}
+                                            error:nil];
+    LVCFileRevision *revision1File2 = [MTLJSONAdapter modelOfClass:LVCFileRevision.class
+                                                fromJSONDictionary:self.validJSON
+                                                             error:nil];
+    revision1File2.file = file2;
+    XCTAssertNotNil(revision1File2, @"revision1File2 should not be nil");
+    XCTAssertNotNil(revision1File2.file, @"should have file");
+
+    XCTAssertNotEqual(revision1File2, revision1File1, @"different pointers");
+    XCTAssertEqualObjects(revision1File2, revision1File1, @"objects equal");
+    XCTAssertEqual(revision1File2.hash, revision1File1.hash, @"same hash");
+    XCTAssertNotEqualObjects(revision1File2.description,
+                             revision1File1.description,
+                             @"Different Descriptions");
 }
 
-- (void)testExample
+
+- (void)testURLPath
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    LVCFile *file = [MTLJSONAdapter modelOfClass:LVCFile.class
+                              fromJSONDictionary:@{@"name": @"foo-bar"}
+                                           error:nil];
+    LVCFileRevision *fileRevision = [MTLJSONAdapter modelOfClass:LVCFileRevision.class
+                                              fromJSONDictionary:self.validJSON
+                                                           error:nil];
+    fileRevision.file = file;
+    XCTAssertNotNil(fileRevision, @"fileRevision should not be nil");
+    XCTAssertNotNil(fileRevision.file, @"should have file");
+
+    XCTAssertEqualObjects(fileRevision.urlPath, @"foo-bar/4",
+                          @"unexpected URL path");
 }
+
 
 @end
