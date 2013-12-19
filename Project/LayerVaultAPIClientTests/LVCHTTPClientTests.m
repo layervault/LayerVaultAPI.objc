ddef7dcf66b0c81eae24123908b6eda69f1220c6
Matt Thomas <matt@codecaffeine.com>
2013-12-19 14:16:49 -0500

    Fleshing out tests

--- Project/LayerVaultAPIClientTests/LVCHTTPClientTests.m
+++ Project/LayerVaultAPIClientTests/LVCHTTPClientTests.m
@@ -7,12 +7,13 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <LayerVaultAPI/LayerVaultAPI.h>
 #import "LVCAsyncHelper.h"
 #import "LVCMockURLConnection.h"
 #import "LVCMockResponses.h"
 
 @interface LVCHTTPClientTests : XCTestCase
-
+@property (nonatomic) LVCHTTPClient *client;
 @end
 
 @implementation LVCHTTPClientTests
@@ -20,18 +21,73 @@
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    [NSURLProtocol registerClass:[LVCMockURLConnection class]];
+    self.client = [[LVCHTTPClient alloc] initWithClientID:@"CLIENT ID"
+                                                   secret:@"CLIENT SECRET"];
 }
 
-- (void)tearDown
+- (void)testValidProjectFromPartialGoodJSON
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+    [LVCMockURLConnection setJSONResponseWithStatusCode:200
+                                         bodyDictionary:LVCValidProjectJSON()];
+
+    __block LVCProject *returnedProject = nil;
+    __block NSError *returnedError = nil;
+    [self.client getProjectWithName:LVCMockProjectName
+              organizationPermalink:LVCMockOrgPermalink
+                         completion:^(LVCProject *project,
+                                      NSError *error,
+                                      AFHTTPRequestOperation *operation) {
+                             returnedProject = project;
+                             returnedError = nil;
+                         }];
+    [LVCAsyncHelper wait:0.1];
+    XCTAssertNotNil(returnedProject, @"should have returned an project");
+    XCTAssertNil(returnedError, @"should not have returned an error");
+
+    __block LVCProject *returnedProject2 = nil;
+    returnedError = nil;
+    [self.client getProjectFromPartial:returnedProject
+                            completion:^(LVCProject *project,
+                                         NSError *error,
+                                         AFHTTPRequestOperation *operation) {
+                                returnedProject2 = project;
+                                returnedError = error;
+                            }];
+    [LVCAsyncHelper wait:0.1];
+    XCTAssertNotNil(returnedProject2, @"should have returned an project");
+    XCTAssertEqualObjects(returnedProject, returnedProject2, @"should be equal objects");
+    XCTAssertNil(returnedError, @"should not have returned an error");
 }
 
-- (void)testExample
+- (void)testNilProjectFromPartialBadJSON
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    [LVCMockURLConnection setJSONResponseWithStatusCode:200
+                                         bodyDictionary:@{@"foo": @"bar"}];
+
+    __block LVCProject *returnedProject = nil;
+    [self.client getProjectWithName:LVCMockProjectName
+              organizationPermalink:LVCMockOrgPermalink
+                         completion:^(LVCProject *project,
+                                      NSError *error,
+                                      AFHTTPRequestOperation *operation) {
+                             returnedProject = project;
+                         }];
+    [LVCAsyncHelper wait:0.1];
+    XCTAssertNil(returnedProject, @"should not have returned an project");
+
+    LVCProject *project = [MTLJSONAdapter modelOfClass:LVCProject.class
+                                    fromJSONDictionary:LVCValidProjectJSON()
+                                                 error:nil];
+    [self.client getProjectFromPartial:project
+                            completion:^(LVCProject *project,
+                                         NSError *error,
+                                         AFHTTPRequestOperation *operation) {
+                                returnedProject = project;
+                            }];
+    [LVCAsyncHelper wait:0.1];
+    XCTAssertNil(returnedProject, @"should not have returned an project");
 }
+
 
 @end
