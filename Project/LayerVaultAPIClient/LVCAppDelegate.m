1ecd65141ddeef559bc6a7228327b03c9e75da12
Matt Thomas <matt@codecaffeine.com>
2013-12-22 16:54:29 -0500

    Remove main window

--- Project/LayerVaultAPIClient/LVCAppDelegate.m
+++ Project/LayerVaultAPIClient/LVCAppDelegate.m
@@ -7,7 +7,6 @@
 //
 
 #import "LVCAppDelegate.h"
-#import "LVConstants.h"
 #import "LVCProjectWindowController.h"
 #import <LayerVaultAPI/LayerVaultAPI.h>
 #import <ReactiveCocoa/ReactiveCocoa.h>
@@ -16,20 +15,7 @@
 #import "LVCMainWindowController.h"
 
 
-NSString *const emailRegEx =
-@"(?:[a-z0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[a-z0-9!#$%\\&'*+/=?\\^_`{|}"
-@"~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\"
-@"x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-"
-@"z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5"
-@"]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-"
-@"9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21"
-@"-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
-
-
 @interface LVCAppDelegate () <NSTableViewDataSource, NSTableViewDelegate>
-@property (weak) IBOutlet NSTextField *emailField;
-@property (weak) IBOutlet NSSecureTextField *passwordField;
-@property (weak) IBOutlet NSButton *loginButton;
 @property (weak) IBOutlet NSTableView *projectsTableView;
 @property (nonatomic, copy) NSArray *dataSource;
 
@@ -47,116 +33,7 @@ NSString *const emailRegEx =
 {
     self.mainWindowController = [[LVCMainWindowController alloc] initWithWindowNibName:@"LVCMainWindowController"];
     [self.mainWindowController showWindow:nil];
-
-    self.client = [[LVCHTTPClient alloc] initWithClientID:LVClientID
-                                                   secret:LVClientSecret];
-    self.credential = [AFOAuthCredential retrieveCredentialWithIdentifier:self.client.serviceProviderIdentifier];
-
-    [self.projectsTableView setTarget:self];
-    [self.projectsTableView setDoubleAction:@selector(doubleClickedTable:)];
-
-    @weakify(self);
-    [RACObserve(self, credential) subscribeNext:^(AFOAuthCredential *credential) {
-        @strongify(self);
-        BOOL invalidCredential = !credential;
-        self.emailField.enabled = invalidCredential;
-        [self.emailField becomeFirstResponder];
-        self.passwordField.enabled = invalidCredential;
-
-        if (credential) {
-            if (credential.expired) {
-                [self.client authenticateUsingOAuthWithPath:@"/oauth/token"
-                                               refreshToken:credential.refreshToken
-                                                    success:^(AFOAuthCredential *refreshedCredential) {
-                                                        @strongify(self);
-                                                        self.credential = refreshedCredential;
-                                                    }
-                                                    failure:^(NSError *error) {
-                                                        @strongify(self);
-                                                        self.credential = nil;
-                                                    }];
-
-            }
-            else {
-                @weakify(self);
-                [AFOAuthCredential storeCredential:credential
-                                    withIdentifier:self.client.serviceProviderIdentifier];
-                [self.client setAuthorizationHeaderWithCredential:credential];
-                [self.client getMeWithCompletion:^(LVCUser *user,
-                                                   NSError *error,
-                                                   AFHTTPRequestOperation *operation) {
-                    @strongify(self);
-                    self.user = user;
-                    if (!user) {
-                        self.credential = nil;
-                    }
-                }];
-            }
-        }
-        else {
-            self.user = nil;
-            [AFOAuthCredential deleteCredentialWithIdentifier:self.client.serviceProviderIdentifier];
-        }
-    } error:^(NSError *error) {
-        NSLog(@"error: %@", error);
-    } completed:^{
-        NSLog(@"completed");
-    }];
-
-    RACSignal *loginEnabledSignal =
-    [RACSignal
-     combineLatest:@[RACObserve(self, credential),
-                     self.emailField.rac_textSignal,
-                     self.passwordField.rac_textSignal]
-     reduce:^(AFOAuthCredential *credential,
-              NSString *email,
-              NSString *password){
-         BOOL validEmail = [[NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegEx] evaluateWithObject:email];
-         return @(credential || (!credential && validEmail && password.length > 0));
-     }];
-
-    [loginEnabledSignal subscribeNext:^(NSNumber *enabled) {
-        @strongify(self);
-        [self.loginButton setEnabled:[enabled boolValue]];
-    }];
-
-    RAC(self, loginButton.title) = [RACObserve(self, credential)
-                                          map:^NSString *(AFOAuthCredential *credential) {
-                                              return credential ? @"Logout" : @"Login";
-                                          }];
-
-    RAC(self, emailField.stringValue) = [RACObserve(self, user)
-                                         map:^NSString *(LVCUser *user) {
-                                             return user.email ?: @"";
-                                         }];
-
-    RAC(self, passwordField.stringValue) = [RACObserve(self, credential)
-                                            map:^NSString *(AFOAuthCredential *credential) {
-                                                return credential ? @"••••••••" : @"";
-                                            }];
-
-    [RACObserve(self, user) subscribeNext:^(LVCUser *user) {
-        @strongify(self);
-        [self setDataSourceForUser:user];
-    }];
-}
-
-- (IBAction)loginPressed:(NSButton *)sender
-{
-    if (self.credential) { // Button was "logout"
-        self.credential = nil;
-    }
-    else {
-        [self.client authenticateWithEmail:self.emailField.stringValue
-                                  password:self.passwordField.stringValue
-                                completion:^(AFOAuthCredential *credential,
-                                             NSError *error) {
-                                    self.credential = credential;
-                                    if (error) {
-                                        NSLog(@"error: %@", error);
-                                    }
-                                }];
-    }
+    [self.mainWindowController.window makeKeyAndOrderFront:self];
 }
 
 - (IBAction)addProjectPressed:(NSButton *)sender
