bdc79c9a245a1095d3ac283b0cc90660cbf23843
Matt Thomas <matt@codecaffeine.com>
2014-02-10 17:01:32 -0500

    Percent encoding tests

--- Project/LayerVaultAPIClientTests/NSString+PercentEncodingTests.m
+++ Project/LayerVaultAPIClientTests/NSString+PercentEncodingTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <LayerVaultAPI/NSString+PercentEncoding.h>
 
 @interface NSString_PercentEncodingTests : XCTestCase
 
@@ -26,9 +27,28 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testPercentEncodingReservedCharacters
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    XCTAssertEqualObjects([@"!#$&'()*+,/:;=?@[]" lv_stringWithFullPercentEncoding],
+                          @"%21%23%24%26%27%28%29%2A%2B%2C%2F%3A%3B%3D%3F%40%5B%5D");
+}
+
+- (void)testUnreservedCharacters
+{
+    NSString *unreserved = @"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.~";
+    XCTAssertEqualObjects([unreserved lv_stringWithFullPercentEncoding], unreserved);
+}
+
+- (void)testCommonCharacters
+{
+    XCTAssertEqualObjects([@" \"%<>\\^`{|}" lv_stringWithFullPercentEncoding],
+                          @"%20%22%25%3C%3E%5C%5E%60%7B%7C%7D");
+}
+
+- (void)testNewline
+{
+    XCTAssertEqualObjects([@"\n" lv_stringWithFullPercentEncoding],
+                          @"%0A");
 }
 
 @end
