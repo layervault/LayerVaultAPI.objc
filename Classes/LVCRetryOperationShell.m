6c0af8f9fac5384a7988622754d7531abd3a253d
Matt Thomas <matt@codecaffeine.com>
2014-05-30 18:40:09 -0400

    RetryOperationShell

---
+++ Classes/LVCRetryOperationShell.m
@@ -0,0 +1,45 @@
+//  Created by Matt Thomas on 5/30/14.
+//  Copyright (c) 2014 LayerVault. All rights reserved.
+
+#import "LVCRetryOperationShell.h"
+#import <AFNetworking/AFNetworking.h>
+#import <AFOAuth2Client/AFOAuth2Client.h>
+#import "NSMutableURLRequest+OAuth2.h"
+
+@implementation LVCRetryOperationShell
+
++ (instancetype)retryOperationShellWithURLRequest:urlRequest
+                                          success:(void (^)(AFHTTPRequestOperation *operation,
+                                                            id responseObject))success
+                                          failure:(void (^)(AFHTTPRequestOperation *operation,
+                                                            NSError *error))failure
+{
+    return [[self alloc] initWithURLRequest:urlRequest
+                                    success:success
+                                    failure:failure];
+}
+
+- (instancetype)initWithURLRequest:(NSURLRequest *)urlRequest
+                           success:(void (^)(AFHTTPRequestOperation *operation,
+                                             id responseObject))success
+                           failure:(void (^)(AFHTTPRequestOperation *operation,
+                                             NSError *error))failure
+{
+    self = [super init];
+    if (self) {
+        _originalRequest = urlRequest;
+        _success = [success copy];
+        _failure = [failure copy];
+    }
+    return self;
+}
+
+- (NSURLRequest *)requestWithBearerToken:(NSString *)bearerToken
+{
+    NSMutableURLRequest *request = self.originalRequest.mutableCopy;
+    [request lvc_setBearerToken:bearerToken];
+    return request;
+}
+
+
+@end
