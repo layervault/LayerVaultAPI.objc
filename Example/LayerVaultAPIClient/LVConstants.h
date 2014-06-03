39bbbeb562122b1876cd64b1b01c4824764c7838
Matt Thomas <matt@codecaffeine.com>
2014-06-03 09:30:06 -0400

    s/project/example

---
+++ Example/LayerVaultAPIClient/LVConstants.h
@@ -0,0 +1,15 @@
+//
+//  LVConstants.h
+//  LayerVaultAPIClient
+//
+//  Created by Matt Thomas on 10/17/13.
+//  Copyright (c) 2013 LayerVault. All rights reserved.
+//
+
+#ifndef LayerVaultAPIClient_LVConstants_h
+#define LayerVaultAPIClient_LVConstants_h
+
+NSString *const LVClientID = @"CLIENT_ID";
+NSString *const LVClientSecret = @"CLIENT_SECRET";
+
+#endif
