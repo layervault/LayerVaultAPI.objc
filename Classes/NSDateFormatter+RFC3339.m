8a5a19a693e07a48c7eee7c6b88bfc3967489b2e
Matt Thomas <matt@codecaffeine.com>
2014-06-23 12:50:22 -0400

    Cache date formatter

--- Classes/NSDateFormatter+RFC3339.m
+++ Classes/NSDateFormatter+RFC3339.m
@@ -8,14 +8,37 @@
 
 #import "NSDateFormatter+RFC3339.h"
 
+static NSCache *LVCDateFormatterCache = nil;
+
 @implementation NSDateFormatter (RFC3339)
 
 + (NSDateFormatter *)lvc_rfc3339DateFormatter
 {
-    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-    dateFormatter.timeZone = [NSTimeZone timeZoneWithAbbreviation:@"UTC"];
-    dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
-    dateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss'Z'";
+    if (!LVCDateFormatterCache) {
+        NSLog(@"No date formatter cache. Creating new.");
+        LVCDateFormatterCache = [[NSCache alloc] init];
+    }
+
+    NSDateFormatter *dateFormatter = nil;
+
+    NSString *queueName = [NSOperationQueue currentQueue].name;
+    if (queueName) {
+        dateFormatter = [LVCDateFormatterCache objectForKey:queueName];
+    }
+
+    if (!dateFormatter) {
+        NSLog(@"No date formatter for queue '%@'. Creating.", queueName);
+        dateFormatter = [[NSDateFormatter alloc] init];
+        dateFormatter.timeZone = [NSTimeZone timeZoneWithAbbreviation:@"UTC"];
+        dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
+        dateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss'Z'";
+
+        if (queueName) {
+            NSLog(@"Saving date formatter cache");
+            [LVCDateFormatterCache setObject:dateFormatter forKey:queueName];
+        }
+    }
+
     return dateFormatter;
 }
 
