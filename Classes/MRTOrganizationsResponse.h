e3bed0eae8d839ceed1f531490e413de17c81533
Matt Thomas <matt@codecaffeine.com>
2014-09-22 08:31:53 -0400

    Updating Organizations

--- Classes/MRTOrganizationsResponse.h
+++ Classes/MRTOrganizationsResponse.h
@@ -19,18 +19,26 @@ typedef enum : NSUInteger {
 @end
 
 @interface MRTOrganizationResponse : MTLModel <MTLJSONSerializing>
-@property (readonly, nonatomic, copy) NSString *organizationID;
+@property (readonly, nonatomic, copy) NSString *uid;
+@property (readonly, nonatomic, copy) NSURL *href;
+@property (readonly, nonatomic, copy) NSDate *dateCreated;
+@property (readonly, nonatomic, copy) NSDate *dateUpdated;
 @property (readonly, nonatomic, copy) NSString *name;
 @property (readonly, nonatomic, copy) NSString *slug;
-@property (readonly, nonatomic) BOOL isFree;
-@property (readonly, nonatomic) NSDate *dateDeleted; /// @note exclude dateDeleted?
-@property (readonly, nonatomic) NSDate *dateUpdated;
-@property (readonly, nonatomic) NSDate *dateCreated;
-@property (readonly, nonatomic) NSDate *dateCancelled; /// @note exclude dateCancelled
 @property (readonly, nonatomic, copy) NSURL *url;
+@property (readonly, nonatomic) BOOL isFree;
+@property (readonly, nonatomic, copy) NSDate *dateDeleted; /// @note exclude dateDeleted?
+@property (readonly, nonatomic, copy) NSDate *dateCancelled; /// @note exclude dateCancelled
 @property (readonly, nonatomic) LVCSyncType syncType;/// @note exclude LVCSyncTypeDropbox
+@property (readonly, nonatomic) BOOL isTrialingWithoutPayment;
+@property (readonly, nonatomic, copy) NSDate *dateTrialEnds;
+
+
+/// from the JSON
 @property (readonly, nonatomic, copy) NSArray *projectIDs;
 @property (readonly, nonatomic, copy) NSArray *administratorIDs;
 @property (readonly, nonatomic, copy) NSArray *editorIDs;
 @property (readonly, nonatomic, copy) NSArray *spectatorIDs;
+
+@property (nonatomic, copy) NSArray *projects;
 @end
--- Classes/MRTOrganizationsResponse.m
+++ Classes/MRTOrganizationsResponse.m
@@ -22,23 +22,30 @@
 
 @implementation MRTOrganizationResponse
 + (NSDictionary *)JSONKeyPathsByPropertyKey {
-    return @{@"organizationID": @"id",
+    return @{@"uid": @"id",
+             @"href": @"href",
+             @"dateCreated": @"created_at",
+             @"dateUpdated": @"updated_at",
              @"name": @"name",
              @"slug": @"slug",
+             @"url": @"url",
              @"isFree": @"is_free",
              @"dateDeleted": @"deleted_at",
-             @"dateUpdated": @"updated_at",
-             @"dateCreated": @"created_at",
              @"dateCancelled": @"cancelled_at",
-             @"url": @"url",
              @"syncType": @"sync_type",
+             @"isTrialingWithoutPayment": @"is_trialing_without_payment",
+             @"dateTrialEnds": @"trial_end_at",
              @"projectIDs": @"links.projects",
              @"administratorIDs": @"links.administrators",
              @"editorIDs": @"links.editors",
              @"spectatorIDs": @"links.spectators"};
 }
 
-+ (NSValueTransformer *)dateDeletedJSONTransformer {
++ (NSValueTransformer *)hrefJSONTransformer {
+    return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
+}
+
++ (NSValueTransformer *)dateCreatedJSONTransformer {
     return [NSValueTransformer valueTransformerForName:LVCRFC3339DateTransformerName];
 }
 
@@ -46,17 +53,20 @@
     return [NSValueTransformer valueTransformerForName:LVCRFC3339DateTransformerName];
 }
 
-+ (NSValueTransformer *)dateCreatedJSONTransformer {
-    return [NSValueTransformer valueTransformerForName:LVCRFC3339DateTransformerName];
++ (NSValueTransformer *)urlJSONTransformer {
+    return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
 }
 
++ (NSValueTransformer *)dateDeletedJSONTransformer {
+    return [NSValueTransformer valueTransformerForName:LVCRFC3339DateTransformerName];
+}
 
 + (NSValueTransformer *)dateCancelledJSONTransformer {
     return [NSValueTransformer valueTransformerForName:LVCRFC3339DateTransformerName];
 }
 
-+ (NSValueTransformer *)urlJSONTransformer {
-    return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
++ (NSValueTransformer *)dateTrialEndsJSONTransformer {
+    return [NSValueTransformer valueTransformerForName:LVCRFC3339DateTransformerName];
 }
 
 + (NSValueTransformer *)syncTypeJSONTransformer {
