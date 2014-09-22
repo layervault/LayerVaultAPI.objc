9d0d08e0dd738eef2c634e168297de576d0103b4
Matt Thomas <matt@codecaffeine.com>
2014-09-22 09:34:28 -0400

    whoops, forgot these in Folders Response

--- Classes/MRTFoldersResponse.h
+++ Classes/MRTFoldersResponse.h
@@ -14,13 +14,17 @@
 
 @interface MRTFolderResponse : MTLModel <MTLJSONSerializing>
 @property (readonly, nonatomic, copy) NSString *uid;
+@property (readonly, nonatomic, copy) NSURL *href;
+@property (readonly, nonatomic, copy) NSDate *dateCreated;
+@property (readonly, nonatomic, copy) NSDate *dateUpdated;
 @property (readonly, nonatomic, copy) NSString *name;
 @property (readonly, nonatomic, copy) NSString *slug;
 @property (readonly, nonatomic, copy) NSURL *url;
+
+/// @note External Resource IDs
 @property (readonly, nonatomic, copy) NSString *projectID;
 @property (readonly, nonatomic, copy) NSString *parentFolderID;
 @property (readonly, nonatomic, copy) NSArray *folderIDs;
 @property (readonly, nonatomic, copy) NSArray *fileIDs;
-
 @end
 
